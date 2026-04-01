a75607a9e84b523d48d8e8c08fbc40e36efdd4d8
knowlegbox <kieriekhenoc@gmail.com>
2026-04-01 13:57:13 +0000

    je suis revenue sur un models simple pour avancé

--- src/agraphql/order/order_schema.py
+++
@@ -1,6 +0,0 @@
-import undine
-
-from . import types
-
-class OrderQueries(undine.RootType):
-    order = undine.Entrypoint(types.OrderType)
--- src/agraphql/user/query.py
+++
@@ -1,7 +0,0 @@
-
-from undine import QueryType
-from api import models
-
-
-class UserQuery(QueryType[models.User], auto=True):
-    pass
