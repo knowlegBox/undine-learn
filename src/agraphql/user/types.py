226a1bbfce428304b2b1a35dbf06f9f0eea08cd5
knowlegbox <kieriekhenoc@gmail.com>
2026-04-03 16:26:11 +0000

    je suis en train de travailler sur les champs calculé

--- src/agraphql/user/types.py
+++ src/agraphql/user/types.py
@@ -2,6 +2,11 @@
 from api import models
 import undine
 
+class UserCartTotalPrice(undine.Calculation):
+
+    def __call__(self, *args, **kwargs):
+        pass
+
 
 class UserFilter(undine.FilterSet[models.User], auto=True):
     pass
@@ -10,4 +15,5 @@ class UserOrder(undine.OrderSet[models.User], auto=True):
     pass
 @undine.relay.Node
 class UserType(undine.QueryType[models.User],filterset=UserFilter, orderset=UserOrder, auto=True):
+    cart_total = undine.Field(UserCartTotalPrice)
     pk = undine.Field(schema_name="id")
