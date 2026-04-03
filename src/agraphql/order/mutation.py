9713776e1355a4892eea070ac117872846f0fd4e
knowlegbox <kieriekhenoc@gmail.com>
2026-04-03 15:19:14 +0000

    je suis en train de travailler sur les champs calculé

--- src/agraphql/order/mutation.py
+++ src/agraphql/order/mutation.py
@@ -4,6 +4,7 @@ from typing import Any
 import graphql
 import undine
 from undine import GQLInfo
+from undine.typing import TModel
 
 from api import models
 from .types import OrderType
@@ -12,11 +13,11 @@ from undine.utils.graphql.type_registry import get_or_create_graphql_object_type
 def calculation_total_price(order_items:dict[str,Any], order:models.Order = None)->decimal.Decimal:
     amount = 0
     for order_item in order_items:
-        product = models.Product.objects.get(pk=order_item["product"])
+        product = models.Product.objects.get(pk=order_item["product"].pk)
         total_price = order_item["quantity"] * order_item["price"]
         models.OrderItem.objects.create(
             order=order,
-            # product=product,
+            product=product,
             quantity=order_item["quantity"],
             price=order_item["price"],
             total_price=total_price
@@ -43,6 +44,7 @@ class OrderCreate(undine.MutationType[models.Order]):
         try:
             # Récupérer les items (doit être une liste car many=True ajouté)
             order_items = input_data.pop("items", None)
+            # print("order_items: ",order_items)
             
             # Récupérer l'utilisateur (on s'assure d'avoir l'instance)
             user_id = input_data.pop("user", None)
@@ -51,8 +53,9 @@ class OrderCreate(undine.MutationType[models.Order]):
                 user_instance = models.User.objects.get(pk=user_id)
             
             # Créer la commande
+            # print("input_data: ",input_data)
             order = models.Order.objects.create(user=user_instance, **input_data)
-            
+            # print(input_data)
             if not order_items:
                 raise ValueError("Une commande doit contenir au moins un article.")
             
@@ -101,5 +104,54 @@ class OrderCreate(undine.MutationType[models.Order]):
 
 
 
-class OrderUpdate(undine.MutationType[models.Order], auto=True):
-    pass
+class OrderUpdate(undine.MutationType[models.Order]):
+    pk = undine.Input(str,schema_name="id",required=True)
+    is_active = undine.Input()
+    is_deleted = undine.Input()
+
+    @classmethod
+    def __mutate__(cls, instance: TModel, info: GQLInfo, input_data: dict[str, Any]) -> Any:
+        try:
+            # print("input_data: ",input_data)
+            order = models.Order.objects.get(pk=input_data["pk"])
+            items = models.OrderItem.objects.filter(order=order)
+            for item in items:
+                # print("item: ",item)
+                item.is_deleted = True
+                item.is_active = False
+                item.save()
+            return {
+                "success": True,
+                "message": f"Commande mise à jour avec succès {order.id}",
+                "instance": order
+            }
+
+        except models.OrderItem.DoesNotExist:
+            return{
+                "success": False,
+                "message": f"Aucun article trouvé pour la commande {order.id}"
+            }
+        except models.Order.DoesNotExist:
+            return{
+                "success": False,
+                "message": f"Commande non trouvée avec l'ID {input_data['pk']}"
+            }
+        except Exception as e:
+            return {
+                "success": False,
+                "message": f"Erreur lors de la mise à jour {e}",
+                "instance": None
+            }
+
+    @classmethod
+    def __output_type__(cls):
+        order_gpl_type = OrderType.__output_type__()
+        fields ={
+            "success": graphql.GraphQLField(graphql.GraphQLNonNull(graphql.GraphQLBoolean)),
+            "message": graphql.GraphQLField(graphql.GraphQLNonNull(graphql.GraphQLString)),
+            "instance": graphql.GraphQLField(order_gpl_type),
+        }
+        return graphql.GraphQLObjectType(
+            name="UpdateOrderResponse",
+            fields=fields
+        )
--- src/agraphql/order/types.py
+++ src/agraphql/order/types.py
@@ -1,6 +1,23 @@
+
 from api import models
 import undine
+from django.db.models import Value, Sum, OuterRef, DecimalField
+from django.db.models.functions import Coalesce
 
+
+
+class UserCartTotal(undine.Calculation[float]):
+    """Calcule le total de toutes les commandes pour l'utilisateur lié à cette commande."""
+    def __call__(self, info:undine.GQLInfo)->undine.DjangoExpression:
+        return Coalesce(
+            models.Order.objects.filter(
+                user=OuterRef("user"),
+                is_active=True,
+                is_deleted=False,
+            ).values("user").annotate( total=Sum("total_price")).values("total"),Value(0.0),
+            output_field=DecimalField(max_digits=12, decimal_places=2),
+        )
+#========================================================================
 class OrderItemFilter(undine.FilterSet[models.OrderItem], auto=True):
     pass
 class OrderItemOrdering(undine.OrderSet[models.OrderItem], auto=True):
@@ -19,3 +36,4 @@ class OrderOrdering(undine.OrderSet[models.Order], auto=True):
 
 class OrderType(undine.QueryType[models.Order],filterset=OrderFilter, orderset=OrderOrdering, auto=True):
     pk = undine.Field(schema_name="id")
+    cart_total = undine.Field(UserCartTotal)
