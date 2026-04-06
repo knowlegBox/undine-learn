03a24daa681be476c0eabb4c91549677f2196d9a
kierie henoc <kieriekhenoc@gmail.com>
2026-04-06 17:02:42 +0000

    ..

--- src/agraphql/user/types.py
+++ src/agraphql/user/types.py
@@ -1,11 +1,20 @@
 
 from api import models
 import undine
+from django.db.models import Value, Sum, OuterRef,DecimalField
+from django.db.models.functions import Coalesce
 
-class UserCartTotalPrice(undine.Calculation):
+class UserCartTotalPrice(undine.Calculation[float]):
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def __call__(self, info: undine.GQLInfo) -> undine.DjangoExpression:
+        return Coalesce(
+            models.Order.objects.filter(
+                user=OuterRef("pk"),
+                is_active=True,
+                is_deleted=False,
+            ).values("user").annotate(total=Sum("total_price")).values("total"), Value(0.0),
+            output_field=DecimalField(max_digits=12, decimal_places=2),
+        )
 
 
 class UserFilter(undine.FilterSet[models.User], auto=True):
@@ -13,7 +22,7 @@ class UserFilter(undine.FilterSet[models.User], auto=True):
 
 class UserOrder(undine.OrderSet[models.User], auto=True):
     pass
-@undine.relay.Node
+#@undine.relay.Node
 class UserType(undine.QueryType[models.User],filterset=UserFilter, orderset=UserOrder, auto=True):
     cart_total = undine.Field(UserCartTotalPrice)
     pk = undine.Field(schema_name="id")
